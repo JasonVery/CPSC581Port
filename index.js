e486e04efa3d4a55eae3bed29f4b8bbfb807e437
JasonVery <jvery2001@gmail.com>
2026-02-13 18:18:37 -0700

    Added Coms

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const introCont = document.querySelector(".introCont");
     const projectCont = document.querySelector(".projectCont");
 
-    
+
 
 
 
@@ -16,18 +16,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Project Card Interactions
     // Will loop through elements and add a class to the elements so we can get some smooth transitions
+    // Each project will also change the glow effect
     proj1BTN.addEventListener("click", function () {
-  
+
 
     });
 
     proj1BTN.addEventListener("click", function () {
-  
+
 
     });
 
     proj1BTN.addEventListener("click", function () {
-  
+
 
     });
 
