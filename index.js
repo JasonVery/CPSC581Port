f1c77657a52da72fcbe91508a574edf0ea64a71d
JasonVery <jvery2001@gmail.com>
2026-02-13 18:14:27 -0700

    Proj 1 Card layout

---
+++ index.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Project Cards
+    const proj1BTN = document.getElementById("proj1");
+    const proj2BTN = document.getElementById("proj2");
+    const proj3BTN = document.getElementById("proj3");
+
+    // Main Wrappers
+    const introCont = document.querySelector(".introCont");
+    const projectCont = document.querySelector(".projectCont");
+
+    
+
+
+
+
+
+    // Project Card Interactions
+    // Will loop through elements and add a class to the elements so we can get some smooth transitions
+    proj1BTN.addEventListener("click", function () {
+  
+
+    });
+
+    proj1BTN.addEventListener("click", function () {
+  
+
+    });
+
+    proj1BTN.addEventListener("click", function () {
+  
+
+    });
+
+
+
+
+
+});
